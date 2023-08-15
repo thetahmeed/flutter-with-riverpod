30243eb943896dffcb94620e4c805e846db84798
Tahmeedul Islam <tahmeed.com.bd@gmail.com>
2023-08-15 21:19:56 +0600

    Update home_page.dart

--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -10,20 +10,21 @@ class HomePage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final counterData = ref.watch(counter);
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: const Text('Roverpod!'),
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Roverpod!'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '$counterData',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                '$counterData',
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-            ],
-          ),
-        ));
+      ),
+    );
   }
 }
