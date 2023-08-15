8e1af03068fd6473dd4bedb728ed4aa1065b6e44
Tahmeedul Islam <tahmeed.com.bd@gmail.com>
2023-08-15 22:20:59 +0600

    Use of StateNotifierProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_with_riverpod/provider/couter_provider.dart';
 import 'package:flutter_with_riverpod/ui/home/home_page.dart';
 
 void main() {
@@ -11,9 +12,14 @@ final counter = Provider<int>((ref) {
   return 99;
 });
 
-// StateProvider is used to access simple mutable object
+// StateProvider is used to access simple mutable objects
 final counter2 = StateProvider<int>((ref) {
   return 100;
+});
+
+// StateNotifierProvider is used to access complex mutable objects
+final counter3 = StateNotifierProvider<MyCounterProvider, int>((ref) {
+  return MyCounterProvider();
 });
 
 class MyApp extends StatelessWidget {
