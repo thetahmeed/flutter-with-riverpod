b836b70552d857d534e6d059f435b2ac8c1e795c
Tahmeedul Islam <tahmeed.com.bd@gmail.com>
2023-08-15 21:09:20 +0600

    Update home_page.dart

--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -1,29 +1,19 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key, required this.title});
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
-  final String title;
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text(widget.title),
+          title: const Text('Roverpod!'),
         ),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              const Text(
-                'You have pushed the button this many times:',
-              ),
               Text(
                 '',
                 style: Theme.of(context).textTheme.headlineMedium,
