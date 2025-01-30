6c412fc4820aaf4981ab5335d3168290fd6279c1
Tahmeedul Islam <tahmeed.com.bd@gmail.com>
2025-01-31 00:35:23 +0600

    Create todo_list_page.dart

---
+++ lib/updated/todo_list/todo_list_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class TodoListPage extends StatefulWidget {
+  const TodoListPage({super.key});
+
+  @override
+  State<TodoListPage> createState() => _TodoListPageState();
+}
+
+class _TodoListPageState extends State<TodoListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Todo List'),
+        elevation: 7,
+      ),
+    );
+  }
+}
