cc5ee359f0e83210ab3b6deeb719ea3a293a8c52
Tahmeedul Islam <tahmeed.com.bd@gmail.com>
2025-01-31 00:35:21 +0600

    Create todo_provider.g.dart

---
+++ lib/updated/todo_list/provider/todo_provider.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'todo_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$todoProviderNotifierHash() =>
+    r'e5a0f041f65317e6c4763ef152e912ff5dbf4be9';
+
+/// See also [TodoProviderNotifier].
+@ProviderFor(TodoProviderNotifier)
+final todoProviderNotifierProvider = AutoDisposeAsyncNotifierProvider<
+    TodoProviderNotifier, List<TodoModel>>.internal(
+  TodoProviderNotifier.new,
+  name: r'todoProviderNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$todoProviderNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$TodoProviderNotifier = AutoDisposeAsyncNotifier<List<TodoModel>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
