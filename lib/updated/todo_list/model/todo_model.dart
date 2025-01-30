9d4292e2054afec35aac404cda81d45822617ff1
Tahmeedul Islam <tahmeed.com.bd@gmail.com>
2025-01-31 00:35:13 +0600

    Create todo_model.dart

---
+++ lib/updated/todo_list/model/todo_model.dart
@@ -0,0 +1,15 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'todo_model.freezed.dart';
+part 'todo_model.g.dart';
+
+@freezed
+class TodoModel with _$TodoModel {
+  factory TodoModel({
+    required String description,
+    required bool completed,
+  }) = _TodoModel;
+
+  factory TodoModel.fromJson(Map<String, dynamic> json) =>
+      _$TodoModelFromJson(json);
+}
