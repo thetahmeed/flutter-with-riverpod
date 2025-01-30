f933773a1992012e0315e3b81647adf057750c21
Tahmeedul Islam <tahmeed.com.bd@gmail.com>
2025-01-31 00:35:04 +0600

    Create activity.g.dart

---
+++ lib/updated/network_request/model/activity.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'activity.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$ActivityImpl _$$ActivityImplFromJson(Map<String, dynamic> json) =>
+    _$ActivityImpl(
+      userId: (json['userId'] as num).toInt(),
+      id: (json['id'] as num).toInt(),
+      title: json['title'] as String,
+      completed: json['completed'] as bool,
+    );
+
+Map<String, dynamic> _$$ActivityImplToJson(_$ActivityImpl instance) =>
+    <String, dynamic>{
+      'userId': instance.userId,
+      'id': instance.id,
+      'title': instance.title,
+      'completed': instance.completed,
+    };
