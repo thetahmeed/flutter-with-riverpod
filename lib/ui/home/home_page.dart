7ee5585c2b3f31283258de1a720b47b7eb25d9d2
Tahmeedul Islam <tahmeed.com.bd@gmail.com>
2025-08-10 23:34:58 +0600

    Update home_page.dart

--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -1,7 +1,7 @@
 import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../../main.dart';
+import '../../main_old.dart';
 
 class HomePage extends ConsumerWidget {
   const HomePage({super.key});
