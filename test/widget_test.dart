444c9d963362a9980e3341d959d871db64572b31
Tahmeedul Islam <tahmeed.com.bd@gmail.com>
2025-08-10 23:35:07 +0600

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_with_riverpod/main.dart';
+import 'package:flutter_with_riverpod/main_old.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
