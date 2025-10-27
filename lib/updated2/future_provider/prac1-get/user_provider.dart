8eb21163bc12c3be3ca0801c4e476b4ca891396e
Tahmeedul Islam <tahmeed.com.bd@gmail.com>
2025-10-28 01:14:58 +0600

    Update user_provider.dart

--- lib/updated2/future_provider/prac1-get/user_provider.dart
+++ lib/updated2/future_provider/prac1-get/user_provider.dart
@@ -1,8 +1,6 @@
 import 'dart:io';
-
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart' as http;
-
 import 'user.dart';
 
 final userProvider = FutureProvider<List<User>>((ref) async {
